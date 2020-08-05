0ad49d8c0d1243a7a01206135c7010cb5258565a
abdulmansour <molate1591@gmail.com>
2020-08-05 11:48:21 -0400

    modified test to find the keyword 'modified' instead of 'learn react'

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Modified
+          I was modified on the 'feature' branch!
         </a>
       </header>
     </div>
